9df2dc1e5fca02f3d8943a8747637396b1c46866
PRINCE KUMAR <bhagatprince011@gmail.com>
2022-07-13 00:41:57 +0530

    Create 35. Search Insert Position (easy).cpp

---
+++ 35. Search Insert Position (easy).cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start=0,end=nums.size()-1;
+        int i=0;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(target>nums[mid]){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return start;
+        
+    }
+};
