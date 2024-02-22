afca1b262557b1a316314030cfade6549d7e4f56
BhagatPrince <bhagatprince011@gmail.com>
2024-02-23 02:08:33 +0530

    new ques

---
+++ 14-LongestCommonPrefix.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string longestCommonPrefix(vector<string>& strs) {
+
+    string first = strs[0];   
+    for (int i = 0; i < strs.size(); i++)
+    {
+         
+    }
+        
+}
+
+int main() {
+    // Your code here
+    vector<string> strs = {"flower","flow","flight"};
+
+    cout<<longestCommonPrefix(strs)<<endl;
+
+    return 0;
+}
